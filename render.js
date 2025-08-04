ebbdbea5bb402e6cb200b0919fc77c82ed66c2d9
Rito <gddorito@outlook.com>
2025-08-04 01:42:57 -0600

    GOVUK Frontend Setup I

---
+++ render.js
@@ -0,0 +1,13 @@
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+
+// Add views and GOV.UK macro path
+nunjucks.configure([
+  'app/views',
+  'node_modules/govuk-frontend/dist'
+]);
+
+const output = nunjucks.render('index.njk');
+
+// Save it
+fs.writeFileSync('public/index.html', output);
